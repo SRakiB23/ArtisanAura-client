b9ea29bf35db2fa97bdfbd2562a20f680b0b6415
Sazzad Bashar Rakib <sazzadbasharrakib@Sazzads-MacBook-Pro.local>
2024-04-25 22:10:20 +0600

    created navbar and setup the routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import AuthProvider from "./providers/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import Root from "./routes/Root";
+import Home from "./pages/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
+  </React.StrictMode>
+);
